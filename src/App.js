a5c9dd97b930e0854c3dad7981381764bea4a684
leelynne <leelynne@gmail.com>
2016-12-22 14:01:26 -0800

    Server side query

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const DEFAULT_QUERY = 'redux';
+
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+
+/*
 const list = [
   {
     title: 'React',
@@ -20,7 +27,7 @@ const list = [
     objectID: 1,
   },
 ];
-
+*/
 const isSearched = (query) => (item) => !query || item.title.toLowerCase().indexOf(query.toLowerCase()) !== -1;
 
 class App extends Component {
@@ -28,36 +35,60 @@ class App extends Component {
     super(props);
 
     this.state = {
-      list: list,
-      query: '',
+      query: DEFAULT_QUERY,
     };
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
+  }
+
+  setSearchTopStories(result) {
+    this.setState({result});
+  }
+
+  fetchSearchTopStories(query) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${query}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result));
+  }
+
+  componentDidMount() {
+    const {query} = this.state;
+    this.fetchSearchTopStories(query);
   }
 
   onSearchChange(event) {
     this.setState({ query: event.target.value });
   }
 
+  onSearchSubmit(event) {
+    const { query } = this.state;
+    this.fetchSearchTopStories(query);
+    event.preventDefault();
+  }
+
   render() {
-    const {query, list} = this.state;
+    const {query, result } = this.state;
 
     return (
       <div className="page">
         <div className="interactions">
-          <Search value={query} onChange={this.onSearchChange}>
+          <Search value={query} onChange={this.onSearchChange} onSubmit={this.onSearchSubmit}>
             Search
           </Search>
         </div>
-        <Table list={list} pattern={query}/>
+        { result && <Table list={result.hits} pattern={query}/>}
       </div>
     );
   }
 }
 
-const Search = ({ value, onChange, children}) => {
+const Search = ({ value, onChange, onSubmit, children}) => {
   return (
-    <form>
-      {children} <input type="text" value={value} onChange={onChange}/>
+    <form onSubmit={onSubmit}>
+      <input type="text" value={value} onChange={onChange}/>
+      <button type="submit">{children}</button>
     </form>
   );
 };
@@ -65,7 +96,7 @@ const Search = ({ value, onChange, children}) => {
 const Table = ({list, pattern}) => {
   return (
     <div className="table">
-      { list.filter(isSearched(pattern)).map((item) => {
+      { list.map((item) => {
         return (
           <div key={item.objectID} className="table-row">
             <span style={{ width: '40%' }}>
